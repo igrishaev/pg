4271cc3e6753bc7900989dafe10922aef7e7b4f3
Ivan Grishaev <ivan@grishaev.me>
2022-11-13 22:16:07 +0300

    updated

---
+++ src/pg/api.clj
@@ -0,0 +1,69 @@
+(ns pg.api
+  "
+  Public client API.
+  "
+  )
+
+
+(defn query []
+  )
+
+
+(defn insert []
+  )
+
+
+(defn insert-batch []
+  )
+
+
+(defn update []
+  )
+
+
+(defn delete []
+  )
+
+
+(defn prepare-statement []
+  )
+
+
+(defmacro with-transaction []
+  )
+
+
+(defmacro with-statement []
+  )
+
+
+(defmacro with-connection []
+  )
+
+
+(defn copy-in []
+  )
+
+
+(defn copy-out []
+  )
+
+
+(defn func-call []
+  )
+
+
+(defn notify []
+  )
+
+
+(defn cancell []
+  )
+
+
+(defn close-statement []
+  )
+
+
+(defn terminate []
+  )
