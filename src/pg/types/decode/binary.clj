4652b67d478ac0389b8b2c84ee205918fac7871b
Ivan Grishaev <ivan@grishaev.me>
2022-11-21 20:45:36 +0300

    updated

--- src/pg/types/decode/binary.clj
+++ src/pg/types/decode/binary.clj
@@ -11,101 +11,143 @@
    [pg.bb :as bb]))
 
 
-(defmulti decode
-  (fn [buf field enc]
-    (:type-id field)))
+(defmacro get-enc [options]
+  `(get ~options :encoding "UTF-8"))
 
 
-(defmethod decode :default
-  [buf field enc]
+(defmulti mm-decode
+  (fn [oid _ _]
+    oid))
+
+
+(defmethod mm-decode :default
+  [_ ^bytes buf _]
   buf)
 
 
-(defmethod decode oid/FLOAT4
-  [^bytes buf field enc]
+(defmethod mm-decode oid/FLOAT4
+  [_ ^bytes buf _]
   (Float/intBitsToFloat (new BigInteger buf)))
 
 
-(defmethod decode oid/TEXT
-  [^bytes buf field enc]
-  (codec/bytes->str buf enc))
+(defmethod mm-decode oid/TEXT
+  [_ ^bytes buf opt]
+  (codec/bytes->str buf (get-enc opt)))
 
 
-(defmethod decode oid/VARCHAR
-  [^bytes buf field enc]
-  (codec/bytes->str buf enc))
+(defmethod mm-decode oid/VARCHAR
+  [_ ^bytes buf opt]
+  (codec/bytes->str buf (get-enc opt)))
 
 
-(defmethod decode oid/BPCHAR
-  [^bytes buf field enc]
-  (codec/bytes->str buf enc))
+(defmethod mm-decode oid/BPCHAR
+  [_ ^bytes buf opt]
+  (codec/bytes->str buf (get-enc opt)))
 
 
-(defmethod decode oid/NAME
-  [^bytes buf field enc]
-  (codec/bytes->str buf enc))
+(defmethod mm-decode oid/NAME
+  [_ ^bytes buf opt]
+  (codec/bytes->str buf (get-enc opt)))
 
 
-(defmethod decode oid/CHAR
-  [^bytes buf field enc]
+(defmethod mm-decode oid/CHAR
+  [_ ^bytes buf _]
   (-> buf (aget 0) char))
 
 
-(defmethod decode oid/FLOAT8
-  [^bytes buf field enc]
+(defmethod mm-decode oid/FLOAT8
+  [_ ^bytes buf _]
   (Double/longBitsToDouble (new BigInteger buf)))
 
 
-(defmethod decode oid/INT2
-  [^bytes buf field enc]
+(defmethod mm-decode oid/INT2
+  [_ ^bytes buf _]
   (.shortValue (new BigInteger buf)))
 
 
-(defmethod decode oid/UUID
-  [^bytes buf field enc]
+(defmethod mm-decode oid/UUID
+  [_ ^bytes buf _]
   (let [bb (bb/wrap buf)
         l1 (bb/read-long8 bb)
         l2 (bb/read-long8 bb)]
     (new UUID l1 l2)))
 
 
-(defmethod decode oid/OID
-  [^bytes buf field enc]
+(defmethod mm-decode oid/OID
+  [_ ^bytes buf _]
   (.intValue (new BigInteger buf)))
 
 
-(defmethod decode oid/INT4
-  [^bytes buf field enc]
+(defmethod mm-decode oid/INT4
+  [_ ^bytes buf _]
   (.intValue (new BigInteger buf)))
 
 
-(defmethod decode oid/INT8
-  [^bytes buf field enc]
+(defmethod mm-decode oid/INT8
+  [_ ^bytes buf _]
   (.longValue (new BigInteger buf)))
 
 
-(defmethod decode oid/MONEY
-  [^bytes buf field enc]
+(defmethod mm-decode oid/MONEY
+  [_ ^bytes buf _]
   (new BigInteger buf))
 
 
-(defmethod decode oid/BYTEA
-  [^bytes buf field enc]
+(defmethod mm-decode oid/BYTEA
+  [_ ^bytes buf _]
   buf)
 
 
-(defmethod decode oid/VOID
-  [^bytes buf field enc])
+(defmethod mm-decode oid/VOID
+  [_ ^bytes buf _])
 
 
 #_
-(defmethod decode oid/NUMERIC
-  [^bytes buf field enc]
+(defmethod mm-decode oid/NUMERIC
+  [_ ^bytes buf _]
   )
 
 
-(defmethod decode oid/BOOL
-  [^bytes buf field enc]
+(defmethod mm-decode oid/BOOL
+  [_ ^bytes buf _]
   (case (aget buf 0)
     0 false
     1 true))
+
+
+(defmethod mm-decode oid/BIT
+  [_ ^bytes buf _]
+  (let [bb
+        (bb/wrap buf)
+
+        bits-count
+        (bb/read-int32 bb)
+
+        bytes-rest
+        (bb/read-rest bb)
+
+        ;; len-bits
+        ;; (alength bits-rest)
+
+        pad-count
+        (- 8 (alength bytes-rest))
+
+        bytes-full
+        (byte-array
+         (concat (repeat pad-count 0) bytes-rest))
+
+        int-value
+        (new BigInteger bytes-full)
+
+        ]
+
+    int-value
+    )
+)
+
+
+(defn decode
+  ([oid buf]
+   (mm-decode oid buf nil))
+  ([oid buf options]
+   (mm-decode oid buf options)))
