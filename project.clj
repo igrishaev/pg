b7fa774f4fee67dd8a3f45a532ee72eb0e7b72e1
Ivan Grishaev <ivan@grishaev.me>
2023-05-06 19:22:57 +0300

    updated

--- project.clj
+++ project.clj
@@ -10,6 +10,9 @@
   {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
    :url "https://www.eclipse.org/legal/epl-2.0/"}
 
+  :deploy-repositories
+  {"releases" {:url "https://repo.clojars.org" :creds :gpg}}
+
   :release-tasks
   [["vcs" "assert-committed"]
    ["test"]
@@ -52,4 +55,8 @@
   :profiles
   {:dev
    {:dependencies
-    [[org.clojure/clojure "1.11.1"]]}})
+    [[org.clojure/clojure "1.11.1"]]
+
+    :global-vars
+    {*warn-on-reflection* true
+     *assert* true}}})
