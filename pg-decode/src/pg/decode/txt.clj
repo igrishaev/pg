8853c702185c86b1e3b3f73ffffa1cb4a46a0139
Ivan Grishaev <ivan@grishaev.me>
2023-06-01 20:50:50 +0300

    updated

--- pg-decode/src/pg/decode/txt.clj
+++ pg-decode/src/pg/decode/txt.clj
@@ -12,75 +12,75 @@
 
 
 (defmulti -decode
-  (fn [oid string]
+  (fn [oid ^String _string _opt]
     oid))
 
 
 (defmethod -decode :default
-  [_ string]
+  [_ string _]
   string)
 
 
 (defmethod -decode oid/uuid
-  [_ string]
+  [_ string _]
   (UUID/fromString string))
 
 
 (defmethod -decode oid/text
-  [_ string]
+  [_ string _]
   string)
 
 
 (defmethod -decode oid/varchar
-  [_ string]
+  [_ string _]
   string)
 
 
 (defmethod -decode oid/char
-  [_ string]
+  [_ string _]
   (first string))
 
 
 (defmethod -decode oid/bool
-  [_ string]
+  [_ string _]
   (case string
     "t" true
     "f" false))
 
 
 (defmethod -decode oid/int2
-  [_ string]
+  [_ string _]
   (Short/parseShort string))
 
 
 (defmethod -decode oid/int4
-  [_ string]
+  [_ string _]
   (Integer/parseInt string))
 
 
 (defmethod -decode oid/int8
-  [_ string]
+  [_ string _]
   (Long/parseLong string))
 
 
 (defmethod -decode oid/float4
-  [_ string]
+  [_ string _]
   (Float/parseFloat string))
 
 
 (defmethod -decode oid/float8
-  [_ string]
+  [_ string _]
   (Double/parseDouble string))
 
 
 #_
 (defmethod -decode oid/xml
-  [_ string]
-  (Double/parseDouble string))
+  [_ string _]
+  ...)
 
 
 (defmethod -decode oid/numeric
-  [_ string]
+  [_ string _]
   (bigdec string))
 
 
@@ -91,17 +91,26 @@
 
 
 (defmethod -decode oid/timestamptz
-  [_ string]
+  [_ string _]
   (->> string
        (.parse dtfz)
        (Instant/from)))
 
 
 (defmethod -decode oid/date
-  [_ string]
+  [_ string _]
   (LocalDate/parse string))
 
 
 (defmethod -decode oid/time
-  [_ string]
+  [_ string _]
   (LocalTime/parse string))
+
+
+(defn decode
+
+  ([oid string]
+   (-decode oid string nil))
+
+  ([oid string opt]
+   (-decode oid string opt)))
