62a5db262ebfb0a2e3385fce0d7312b71242dd13
Ivan Grishaev <ivan@grishaev.me>
2023-07-10 11:10:51 +0300

    updated

--- pg-client/src/pg/client/pool.clj
+++ pg-client/src/pg/client/pool.clj
@@ -4,37 +4,67 @@
   (:import
    java.util.concurrent.TimeUnit
    java.util.Map
+   java.util.HashMap
+   java.util.concurrent.BlockingQueue
    java.util.concurrent.ArrayBlockingQueue))
 
 
 (defprotocol IPool
+  (idle-full? [this])
+  (idle-empty? [this])
   (borrow-connection [this])
   (return-connection [this conn]))
 
 
+(def QUEUE_TIMEOUT 2000)
+
+
+(defn pull-conn [^BlockingQueue bq]
+  (loop []
+    (or (.poll bq QUEUE_TIMEOUT TimeUnit/MILLISECONDS)
+        (recur))))
+
+
+(defn push-conn [^BlockingQueue bq conn]
+  (loop []
+    (or (.offer bq conn QUEUE_TIMEOUT TimeUnit/MILLISECONDS)
+        (recur))))
+
+
+(defn maybe-refresh-conn [conn]
+  (if (expired? conn)
+    (do (api/terminate conn)
+        (api/clone conn))
+    conn))
+
+
 (deftype Pool
-    [^Long max-size
-     ^Long min-size
+    [^Map pg-config
+     ^Long max-idle
      ^Long max-lifetime
      ^Object sentinel
-     ^ArrayBlockingQueue conns-free
-     ^ArrayBlockingQueue conns-busy]
+     ^BlockingQueue conns-idle
+     ^Map conns-busy]
 
   IPool
 
-  (borrow-connection [this]
+  (idle-full? [this]
+    (>= (.size conns-idle) max-idle))
 
-    ;; of expired?
-    ;; close conn
-    ;; create conn
+  (idle-empty? [this]
+    (zero? (.size conns-idle)))
+
+  (borrow-connection [this]
 
     (locking sentinel
 
       (let [conn
-            (.poll conns-free 20000 TimeUnit/MILLISECONDS)
+            (if (idle-empty? this)
+              (api/connect pg-config)
+              (-> (pull-conn conns-idle)
+                  (maybe-refresh-conn)))]
 
-            result
-            (.offer conns-busy conn 20000 TimeUnit/MILLISECONDS)]
+        (.put conns-busy "foo" conn)
 
         conn)))
 
@@ -42,31 +72,51 @@
 
     (locking sentinel
 
-      ;; if expired?
-      ;; close conn
-      ;; create conn
+      (.remove conns-busy "foo")
 
-      (let [flag
-            (.remove conns-busy conn)
+      (if (idle-full? this)
 
-            flag
-            (.offer conns-busy conn 20000 TimeUnit/MILLISECONDS)]
+        (api/terminate conn)
 
-        nil))))
+        (let [conn (maybe-refresh-conn conn)]
+
+          (push-conn conns-idle conn)))
+
+      nil)))
+
+
+(defmacro with-connection [[bind pool] & body]
+  `(let [~bind (borrow-connection ~pool)]
+     (try
+       ~@body
+       (finally
+         (return-connection ~pool ~bind)))))
 
 
 
-(defn make-pool [config]
+(def pool-defaults
+  {:max-idle 4
+   :max-lifetime 999999999999})
 
-  (let [{:keys [max-size
-                min-size
-                max-lifetime]}
-        config]
 
-    (new Pool
-         max-size
-         min-size
-         max-lifetime
-         (new Object)
-         (new ArrayBlockingQueue max-size)
-         (new ArrayBlockingQueue max-size))))
+(defn make-pool
+
+  ([pg-config]
+   (make-pool pg-config nil))
+
+  ([pg-config pool-config]
+
+   (let [pool-config+
+         (merge pool-defaults pool-config)
+
+         {:keys [max-idle
+                 max-lifetime]}
+         pool-config+]
+
+     (new Pool
+          pg-config
+          max-idle
+          max-lifetime
+          (new Object)
+          (new ArrayBlockingQueue max-idle)
+          (new HashMap)))))
