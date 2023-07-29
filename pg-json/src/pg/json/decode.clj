7c0d94e0186ee8ffd8397e31e52e9da1135c1e68
Ivan Grishaev <ivan@grishaev.me>
2023-07-29 20:26:52 +0300

    json refactored

--- pg-json/src/pg/json/decode.clj
+++ pg-json/src/pg/json/decode.clj
@@ -1,4 +1,35 @@
 (ns pg.json.decode
   (:require
-   pg.json.decode.bin
-   pg.json.decode.txt))
+   [cheshire.core :as json]
+   [pg.oid :as oid]
+   [pg.decode.txt :as txt]
+   [pg.decode.bin :as bin]))
+
+
+;;
+;; Txt
+;;
+
+(txt/expand [oid/json oid/jsonb]
+  [string _ _]
+  (json/parse-string string keyword))
+
+
+;;
+;; Bin
+;;
+
+(defn get-server-encoding ^String [opt]
+  (get opt :server-encoding "UTF-8"))
+
+
+(bin/expand [oid/json oid/jsonb]
+  [^bytes buf _ opt]
+
+  (let [encoding
+        (get-server-encoding opt)
+
+        string
+        (new String buf encoding)]
+
+    (json/parse-string string keyword)))
--- pg-json/src/pg/json/encode.clj
+++ pg-json/src/pg/json/encode.clj
@@ -1,4 +1,52 @@
-(ns pg.json.encode
+n(ns pg.json.encode
   (:require
-   pg.json.encode.bin
-   pg.json.encode.txt))
+   [cheshire.core :as json]
+   [pg.oid :as oid]
+   [pg.encode.txt :as txt]
+   [pg.encode.bin :as bin]))
+
+
+;;
+;; Txt
+;;
+
+(txt/expand [Object oid/json
+             Object oid/jsonb]
+  [obj _ _]
+  (json/generate-string obj))
+
+
+(txt/expand [String oid/json
+             String oid/jsonb]
+  [string _ _]
+  string)
+
+
+;;
+;; Bin
+;;
+
+(defn get-client-encoding ^String [opt]
+  (get opt :client-encoding "UTF-8"))
+
+
+(bin/expand [Object oid/json
+             Object oid/jsonb]
+  [obj _ opt]
+
+  (let [encoding
+        (get-client-encoding opt)]
+
+    (-> obj
+        (json/generate-string obj)
+        (.getBytes encoding))))
+
+
+(bin/expand [String oid/json
+             String oid/jsonb]
+  [^String string _ opt]
+
+  (let [encoding
+        (get-client-encoding opt)]
+
+    (.getBytes string encoding)))
