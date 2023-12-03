e8b082192948c06cfa6fa0698649b49b2263c05a
Ivan Grishaev <ivan@grishaev.me>
2023-12-03 20:04:36 +0300

    updated

--- pg-client/java/com/github/igrishaev/codec/EncoderTxt.java
+++ pg-client/java/com/github/igrishaev/codec/EncoderTxt.java
@@ -3,6 +3,7 @@ package com.github.igrishaev.codec;
 import clojure.lang.Symbol;
 import com.github.igrishaev.PGError;
 import com.github.igrishaev.enums.OID;
+import java.util.UUID;
 
 public class EncoderTxt extends ACodec {
 
@@ -13,9 +14,25 @@ public class EncoderTxt extends ACodec {
     public String encode(Object x, OID oid) {
         return switch (x) {
 
+            case Symbol s -> {
+                switch (oid) {
+                    case TEXT, VARCHAR: yield s.toString();
+                    default: yield encodingError(x, oid);
+                }
+            }
+
             case String s -> {
                 switch (oid) {
                     case TEXT, VARCHAR: yield s;
+                    default: yield encodingError(x, oid);
+                }
+            }
+
+            case Short s -> {
+                switch (oid) {
+                    case INT2: yield String.valueOf(s);
+                    case INT4: yield String.valueOf(s.intValue());
+                    case INT8: yield String.valueOf(s.longValue());
                     default: yield encodingError(x, oid);
                 }
             }
@@ -26,66 +43,52 @@ public class EncoderTxt extends ACodec {
                     case INT4: yield String.valueOf(i);
                     case INT8: yield String.valueOf(i.longValue());
                     default: yield encodingError(x, oid);
-
                 }
             }
 
-            // case Long l -> "2222";
+            case Long l -> {
+                switch (oid) {
+                    case INT2: yield String.valueOf(l.shortValue());
+                    case INT4: yield String.valueOf(l.intValue());
+                    case INT8: yield String.valueOf(l);
+                    default: yield encodingError(x, oid);
+                }
+            }
 
-            default -> throw new PGError("aaa");
-        };
-    }
+            case Float f -> {
+                switch (oid) {
+                    case FLOAT4: yield String.valueOf(f);
+                    case FLOAT8: yield String.valueOf(f.doubleValue());
+                    default: yield encodingError(x, oid);
+                }
+            }
 
-    // Symbol
-    public String encode(Symbol x, OID oid) {
-        return switch (oid) {
-            case TEXT, VARCHAR -> x.toString();
+            case Double d -> {
+                switch (oid) {
+                    case FLOAT4: yield String.valueOf(d.floatValue());
+                    case FLOAT8: yield String.valueOf(d);
+                    default: yield encodingError(x, oid);
+                }
+            }
+
+            case UUID u -> {
+                switch (oid) {
+                    case TEXT, VARCHAR: yield String.valueOf(u);
+                    default: yield encodingError(x, oid);
+                }
+            }
+
+            case Boolean b -> {
+                switch (oid) {
+                    case BOOL: yield b ? "t" : "f";
+                    default: yield encodingError(x, oid);
+                }
+            }
+
             default -> encodingError(x, oid);
         };
     }
 
-    // Character
-    public String encode(char x, OID oid) {
-        return switch (oid) {
-            case CHAR, TEXT, VARCHAR -> String.valueOf(x);
-            default -> encodingError(x, oid);
-        };
-    }
-
-    // String
-//    public String encode(String x, OID oid) {
-//        return switch (oid) {
-//            case TEXT, VARCHAR -> x;
-//            default -> encodingError(x, oid);
-//        };
-//        if (oid == null || oid == OID.INT2 || oid == OID.INT4 || oid == OID.INT8) {
-//            return x.toString();
-//        }
-//        else {
-//            throw new PGError("Encoding error, value: %s, oid:", x, oid);
-//        }
-//    }
-
-    //
-    // Long
-    //
-
-
-    //
-    // Integer
-    //
-
-    //
-    // Short
-    //
-
-    //
-    // Double
-    //
-
-    //
-    // Float
-    //
 
     //
     // BigDecimal
@@ -98,21 +101,7 @@ public class EncoderTxt extends ACodec {
     //
     // BigInt
     //
-
-    //
-    // Boolean
-    //
-    public String encode(Boolean x) {
-        return encode(x, null);
-    }
-
-    public String encode(Boolean x, OID oid) {
-        return switch (oid) {
-            case OID.BOOL -> x ? "t" : "f";
-            default -> throw new PGError("Encoding error, value: %s, oid:", x, oid);
-        };
-    }
-
+    
     //
     // OID
     //
@@ -121,8 +110,6 @@ public class EncoderTxt extends ACodec {
     // Name
     //
 
-    //
-    // UUID
-    //
+
 
 }
